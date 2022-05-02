6c7d124d9336af4db8d9fe8b57008b10e9ddad29
사용자이름 <>
2022-05-02 18:12:42 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express"
-import bodyParser from 'body-parser'
-import mysql from 'mysql'
+import express from "express";
+import bodyParser from 'body-parser';
+import mysql from 'mysql';
 
-const app = express()// express 객체
-const port: string | number = process.env.PORT || 5000
+const app = express(); // express 객체
+const port: string | number = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const pool = mysql.createPool({
   connectionLimit:10, //최대 커넥션 갯수
@@ -16,21 +16,41 @@ const pool = mysql.createPool({
   database: 'nodejs'
 })
 
+class Inform {
+  id: number = 0;
+  name: string = "";
+  age: number = 0;
+  gender: string = "";
+
+  constructor(id: number, name: string, age: number, gender: string) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+
+let informList: Inform[] = [];
+
 app.get('', (req: express.Request, res: express.Response) => {
-  pool.getConnection((err: Error, Connection: any) => {
-    if(err) throw err
-    console.log(`connected as id ${Connection.threadId}`)
+  res.send(informList);
+})
 
-    Connection.query('SELECT * from inform', (err: Error, rows: express.Request) => {
-      Connection.release() //pool을 반납
+//add
+app.post('', (req: express.Request, res: express.Response) => {
 
-      if(!err){
-        res.send(rows)
-      }else {
-        console.log(err)
-      }
-    })
-  })
+  const a:Inform = req.body;
+
+  if(a.id && a.name && a.age && a.gender) {
+    informList.push(a);
+    res.send(informList);
+    console.log(informList);
+      
+  } else {
+    res.send("올바른 값을 입력하세요");
+  }
+  
 })
 
 //get 
@@ -69,25 +89,7 @@ app.delete('/:id', (req: express.Request, res: express.Response) => {
   })
 })
 
-//add
-app.post('', (req: express.Request, res: express.Response) => {
-  pool.getConnection((err: Error, Connection: any) => {
-    if(err) throw err
-    console.log(`connected as id ${Connection.threadId}`)
 
-    Connection.query('INSERT INTO inform SET ?',req.body, (err: Error) => {
-      Connection.release()
-
-      if(!err){
-        res.send(`Record ID: ${req.body.id} has been added`)
-      }else {
-        console.log(err)
-      }
-    })
-
-    console.log(req.body)
-  })
-})
 
 //Update
 app.put('', (req: express.Request, res: express.Response) => {
